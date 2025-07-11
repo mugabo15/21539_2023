92c7540985194db8303d925d573ff9a2d534eb22
mugabo15 <smugabo@15gmail.com>
2025-07-11 09:47:41 +0200

    Changes

--- math.js
+++ math.js
@@ -1,5 +1,6 @@
 export const add = (a, b) => a + b;
 export const multiply = (a, b) => a * b;
+// export const multiply = (a, b) => a * b;
 export const divide = (a, b) => {
   if (b === 0) throw new Error('Division by zero');
   return a / b;
